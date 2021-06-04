f8f66badf9913cb98eeb3750a384b933c8221e30
Canoua <akariaka@yandex.ru>
2021-06-04 20:17:15 +0600

    04.06 20:17

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\Location;
 
 class User extends Authenticatable
 {
@@ -41,9 +40,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-   // public function Locations()
-    //{
-   // 	return $this->hasMany(Location::class);
-    //}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LocationsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,16 +21,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/location',[LocationsController::class, 'add']);
-Route::post('/location',[LocationsController::class, 'create']);
+Auth::routes();
 
-Route::get('/location/{location}', [LocationsController::class, 'edit']);
-Route::post('/location/{location}', [LocationsController::class, 'update']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Auth::routes();
+Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-//Auth::routes();
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
